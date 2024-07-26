73c6e3a5953bfdc0b4d9fc9438dc50dee625b7e0
Alex <150617937+ChuddOff@users.noreply.github.com>
2024-07-26 23:02:21 +0300

    cost

--- src/app/api/getCost/route.ts
+++ src/app/api/getCost/route.ts
@@ -2,7 +2,7 @@ import ccxt from "ccxt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const exchange = new ccxt.binance();
+  const exchange = new ccxt.bigone();
 
   try {
     const url = new URL(req.url || "");
--- src/app/api/getData/route.ts
+++ src/app/api/getData/route.ts
@@ -2,19 +2,13 @@ import ccxt from "ccxt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  console.log(1);
-
   const exchange = new ccxt.bigone();
 
   try {
-    console.log(exchange.id);
     const url = new URL(req.url || "");
     const params = new URLSearchParams(url.searchParams);
-    console.log(url);
     const type = params.get("type");
-    console.log(type);
     const orderBook = await exchange.fetchOrderBook(type as string);
-    console.log(5);
     return NextResponse.json({
       status: 200,
       order: orderBook,
