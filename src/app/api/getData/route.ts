7ba9f582a2517a1a4f3a6339ebd015f974011de2
Alex <150617937+ChuddOff@users.noreply.github.com>
2024-07-26 21:12:12 +0300

    fix

--- src/app/api/getData/route.ts
+++ src/app/api/getData/route.ts
@@ -2,16 +2,19 @@ import ccxt from "ccxt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
+  console.log(1);
+
   const exchange = new ccxt.binance();
 
   try {
+    console.log(2);
     const url = new URL(req.url || "");
     const params = new URLSearchParams(url.searchParams);
-
+    console.log(3);
     const type = params.get("type");
-
+    console.log(4);
     const orderBook = await exchange.fetchOrderBook(type as string);
-
+    console.log(5);
     return NextResponse.json({
       status: 200,
       order: orderBook,
